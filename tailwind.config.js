412e7a3443cd23104f3c43e034080d8cedf6f3af
vvlong1801 <long.vv184146@sis.hust.edu.vn>
2023-06-12 23:21:33 +0700

    done CRUD muscle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const colors = require('tailwindcss/colors');
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.yellow,
+        neutral: colors.gray
+      }
+    }
   },
   plugins: []
 };
