ef339566d9fcfbf45ed467b9b0bc5a1c1cead796
vvlong1801 <long.vv184146@sis.hust.edu.vn>
2023-06-12 22:39:08 +0700

    done auth, login service

---
+++ src/bootstrap.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useStorage } from "@vueuse/core";
+
+window.axios = axios;
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+const accessToken = useStorage("access_token", "");
+
+window.axios.defaults.headers.common[
+  "Authorization"
+] = `Bearer ${accessToken.value}`;
+
+axios.interceptors.response.use(
+  (response) => response.data,
+  (err) => {
+    if (err.response?.status === 401) {
+      accessToken.value = null;
+      axios.defaults.headers.common["Authorization"] = "Bearer";
+      // eslint-disable-next-line no-undef
+      redirect("/auth/login");
+    }
+  }
+);
